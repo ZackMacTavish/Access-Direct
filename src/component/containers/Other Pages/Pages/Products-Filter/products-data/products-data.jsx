17e28e36eb6b3f088cce0b0a6c8352ba6d1487dc
ZackMacTavish <zmactavish@gmail.com>
2021-01-27 19:49:22 -0500

    added more products to products page

--- src/component/containers/Other Pages/Pages/Products-Filter/products-data/products-data.jsx
+++ src/component/containers/Other Pages/Pages/Products-Filter/products-data/products-data.jsx
@@ -7,7 +7,6 @@ export const PRODUCT_DATA =
     {
         company: 'RCI',
         name: '0563',
-        description: '',
         image: require('../product photos/0563.png'),
         description: 'A recommended Access Direct Product' ,
         id: '0'
@@ -131,6 +130,134 @@ export const PRODUCT_DATA =
         image: require('../product photos/Hinge.png'),
         description: 'A recommended Access Direct Product.' ,
         id: '15'
+    },
+
+    {
+        company: 'Camden',
+        name: 'CX-ED1259L',
+        image: require('../product photos/CX-ED1259L-wide.jpg'),
+        description: 'A Rim panic strike that is surface mounted.' ,
+        id: '16'
+    },
+
+    {
+        company: 'CRP',
+        name: 'BB31',
+        image: require('../product photos/BB31.png'),
+        description: '26D 4 1/2 x 4 1/2 Non-NRP Non-ball-bearing Hinge. Screws come with 2 sets (One for metal and one for wood)' ,
+        id: '17'
+    },
+
+    {
+        company: 'CRP',
+        name: 'BB5200',
+        image: require('../product photos/BB5200.jpg'),
+        description: 'Commercial Standard weight 4 ½ x 4 ½ NRP Ball-bearing 26D (Brass/Steel chrome plated) Hinge. Available in Residential/Standard/Heavy Weights.' ,
+        id: '18'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'Collars',
+        image: require('../product photos/Collars.jpg'),
+        description: 'Collars for different length of housing. Left collar is a security collar, and has a 7 pin housing. The middle collar is 6 pin, and the end piece is for varying use.' ,
+        id: '19'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'D6',
+        image: require('../product photos/D6.jpg'),
+        description: 'Small format 6 pin IC core. Available as a 20 pack or individual.' ,
+        id: '20'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'D7',
+        image: require('../product photos/D7.jpg'),
+        description: '7 pin IC core. Pins are placed in the top to set the lock. Key is placed into slot in the front.' ,
+        id: '21'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'DQA',
+        image: require('../product photos/DQA.jpg'),
+        description: 'IC nicket silver. Color determines hardness of the key.' ,
+        id: '22'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'ICM-7-26D-AR',
+        image: require('../product photos/ICM-7-26D-AR.jpg'),
+        description: 'IC housing, screw secures housing in place.' ,
+        id: '23'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'ICM-7-26D-ST',
+        image: require('../product photos/ICM-7-26D-ST.png'),
+        description: 'Small format IC standard cam.' ,
+        id: '24'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'ICR-7-26D',
+        image: require('../product photos/ICR-7-26D.jpg'),
+        description: 'IC small format 7 pin, mortise housing that can pair with multiple lock sets. Can work with multiple collars. This is used for a panic device, mortise lock, or an Adams Rite latch.' ,
+        id: '25'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'Tailpiece-IC',
+        image: require('../product photos/Tailpiece-IC.png'),
+        description: 'Yale is top left, Sargent is top right, Adams Rite is bottom left, and Falcon is bottom right.' ,
+        id: '26'
+    },
+
+    {
+        company: 'GMS/KSP',
+        name: 'Tailpiece-Non-IC',
+        image: require('../product photos/Non-IC.png'),
+        description: 'Tailpiece for housing. Tailpieces for Adams Rite, Yale, Falcon, and Schlage keyway.' ,
+        id: '27'
+    },
+
+    {
+        company: 'Hager',
+        name: '1751 REV.P',
+        image: require('../product photos/1751-REV.png'),
+        description: 'Hager Spring Hinge Round-Cornered Residential weight.' ,
+        id: '28'
+    },
+
+    {
+        company: 'Hager',
+        name: '10196',
+        image: require('../product photos/10196.jpg'),
+        description: 'Electrified 2 ball bearing hinge - 4 1/2 x 4 1/2 inch square edge. US3 hinge - bright brass.' ,
+        id: '29'
+    },
+
+    {
+        company: 'Highpower',
+        name: 'Single Gang Keyswitch',
+        image: require('../product photos/SingleGangKeyswitch.png'),
+        description: 'Medeco key switch.' ,
+        id: '30'
+    },
+
+    {
+        company: 'Highpower',
+        name: 'Medeco Key Switch (Narrow)',
+        image: require('../product photos/Narrow-singlegang.png'),
+        description: 'Narrow single gang keyswitch.' ,
+        id: '31'
     },
 
     ]
