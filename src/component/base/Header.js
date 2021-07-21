e3b4c5fd750049cadf05f526b103e50822856ad2
ZackMacTavish <zmactavish@gmail.com>
2021-07-21 13:03:28 -0700

    added nav menu drop down

--- src/component/base/Header.js
+++ src/component/base/Header.js
@@ -16,7 +16,16 @@ class Header extends React.Component {
                         <ul>
                             <li><a href={AD_HOME}>HOME</a></li>
                             <li><a href={AD_COMPANY}>COMPANY</a></li>
-                            <li><a href={AD_OUR_PRODUCT}>OUR PRODUCTS</a></li>
+                            <li><a href={AD_OUR_PRODUCT}>OUR PRODUCTS</a>
+                                <ul className="our-products-dropdown">
+                                <li><a href="/products">All Products</a></li>
+                                <li><a href="/Highpower">Highpower</a></li>
+                                <li><a href="#">Pneumatic Switches</a></li>
+                                <li><a href="#">Fobs/Cards</a></li>
+                                <li><a href="#">Record Usa</a></li>
+                                <li><a href="/Zamakast">Zamakast</a></li>
+                                </ul>                            
+                            </li>
                             <li><a href={AD_TERMS}>TERMS</a></li>
                             <li><a href={AD_CONTACT}>CONTACT</a></li>
                         </ul>
