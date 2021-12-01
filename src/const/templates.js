a435fa3cd3b360d11f4764b7acf6432cbd4fc0c5
Zachary MacTavish <zacharymactavish@Zacharys-MacBook-Pro.local>
2021-12-01 15:51:14 -0800

    added in a few pictures for fob template picker

--- src/const/templates.js
+++ src/const/templates.js
@@ -2,6 +2,12 @@ import imageblack from './black_fob_1.png';
 import image2 from './black_fob_2.png';
 import image3 from './black_fob_3.png';
 import image4 from './black_fob_4.png';
+import blackcover from './BlackFoboCover.png';
+import redfob from './red_fob.png';
+import redfobcover from './red_fob_cover.png';
+import grayfob from './gray_fob.png';
+import grayfobcover from './GrayFoboCover.png';
+
 
 export const BLACK_FOB_BASE_1 = imageblack;
 export const BLACK_FOB_BASE_2 = image2;
@@ -9,8 +15,8 @@ export const BLACK_FOB_BASE_3 = image3;
 export const BLACK_FOB_BASE_4 = image4;
 export const BLACK_FOB_BASE_5 = "https://access-direct-resource.s3.us-east-2.amazonaws.com/template/black_fob_5.png";
 export const BLACK_FOB_BASE_6 = "https://access-direct-resource.s3.us-east-2.amazonaws.com/template/black_fob_6.png";
-export const BLACK_FOB_COVER = imageblack;
-export const RED_FOB_BASE = "https://access-direct-resource.s3.us-east-2.amazonaws.com/template/red_fob.png";
-export const RED_FOB_COVER = "https://access-direct-resource.s3.us-east-2.amazonaws.com/template/red_fob_cover.png";
-export const GRAY_FOB_BASE = "https://access-direct-resource.s3.us-east-2.amazonaws.com/template/gray_fob.png";
-export const GRAY_FOB_COVER = "https://access-direct-resource.s3.us-east-2.amazonaws.com/template/gray_fob_cover.png";
+export const BLACK_FOB_COVER = blackcover;
+export const RED_FOB_BASE = redfob;
+export const RED_FOB_COVER = redfobcover;
+export const GRAY_FOB_BASE = grayfob;
+export const GRAY_FOB_COVER = grayfobcover;
